b5bacff10b24259fcde4d7ce0711fda351267a7d
Sander Koenders <sanderkoenders@gmail.com>
2020-06-09 19:15:04 +0200

    Refactor Calendar API

--- src/google/CalendarApi.ts
+++ src/google/CalendarApi.ts
@@ -1,38 +1,52 @@
-import fetch from 'node-fetch';
+import { ParsedUrlQueryInput, stringify } from 'querystring';
+import Transporter from '../Transporter';
 
 class CalendarApi {
-    private accessToken: string;
+  private accessToken: string;
+  private transporter: Transporter;
 
-    constructor(accessToken: string) {
-        this.accessToken = accessToken;
+  constructor(accessToken: string, transporter: Transporter) {
+    this.accessToken = accessToken;
+    this.transporter = transporter
+  }
+
+  private static readonly CALENDAR_BASE_URL = 'https://www.googleapis.com/calendar/v3';
+
+  public getEvents(timeMin: string, timeMax: string) {
+    const query = {
+      timeMin: timeMin,
+      timeMax: timeMax,
+      orderBy: 'startTime',
+      singleEvents: true
+    };
+
+    return this.getCalendarIds()
+      .then(calendarIds => calendarIds.map(calendarId => this.getEventsForCalendarId(calendarId, query)))
+      .then(eventsPromises => Promise.all(eventsPromises))
+      .then(eventsResponses => eventsResponses.flatMap(events => events));
+  }
+
+  private getCalendarIds(): Promise<string[]> {
+    return this.sendGetRequest(`${CalendarApi.CALENDAR_BASE_URL}/users/me/calendarList`)
+      .then(calendarResponse => calendarResponse.items.map((calendar: any) => calendar.id));
+  }
+
+  private getEventsForCalendarId(calendarId: string, opts: ParsedUrlQueryInput) {
+    return this.sendGetRequest(`${CalendarApi.CALENDAR_BASE_URL}/calendars/${encodeURIComponent(calendarId)}/events?${stringify(opts)}`)
+      .then(eventsResponse => eventsResponse.items)
+  }
+
+  private sendGetRequest(url: string) {
+    const requestInit = {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${this.accessToken}`
+      }
     }
 
-    getCalendarList() {
-        const requestOpts = {
-            headers: this.getAuthorizationHeader()
-        }
-
-        return fetch('https://www.googleapis.com/calendar/v3/users/me/calendarList', requestOpts)
-            .then(response => response.json());
-    }
-
-    getCalendarEvents(calendarId: string, opts?: string) {
-        const requestOpts = {
-            headers: this.getAuthorizationHeader()
-        }
-
-        return fetch(`https://www.googleapis.com/calendar/v3/calendars/${ calendarId }/events?${ opts }`, requestOpts)
-            .then(response => response.json())
-            .then(eventsResponse => eventsResponse.items)
-    }
-
-    getCalendarEventsForToday(calendarId: string) {
-        return this.getCalendarEvents(calendarId, 'timeMin=2020-06-03T00:00:00Z&timeMax=2020-06-03T23:59:59Z&orderBy=startTime&singleEvents=true')
-    }
-
-    private getAuthorizationHeader() {
-        return { 'Authorization': `Bearer ${ this.accessToken }` };
-    }
+    return this.transporter(url, requestInit)
+      .then(response => response.json())
+  }
 }
 
 export default CalendarApi;
