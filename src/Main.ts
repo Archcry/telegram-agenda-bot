fe77365f2fd4bb8850dcf7aa6821e9344165b15d
Sander Koenders <hello@sanderkoenders.nl>
2024-02-13 14:55:24 +0100

    Do not send messages at startup

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
-import moment from 'moment-timezone';
+import fetch from "node-fetch";
+import moment from "moment-timezone";
 
-import GoogleOAuth2 from './google/GoogleOAuth2';
-import CalendarApi from './google/CalendarApi';
-import Telegram from './telegram/Telegram';
+import GoogleOAuth2 from "./google/GoogleOAuth2";
+import CalendarApi from "./google/CalendarApi";
+import Telegram from "./telegram/Telegram";
 
-import * as os from 'os';
+import * as os from "os";
 
 class Main {
   private authApi: GoogleOAuth2;
@@ -16,16 +16,12 @@ class Main {
 
   constructor(authApi: GoogleOAuth2, telegramApi: Telegram, users: any[]) {
     this.authApi = authApi;
-    this.telegramApi = telegramApi
+    this.telegramApi = telegramApi;
 
     this.users = users;
   }
 
   public start(callback?: (interval: number) => void) {
-    this.users.forEach((user: any) => {
-      this.sendCalendarEvents(user);
-    });
-
     setInterval(this.checkUsers.bind(this), Main.INTERVAL);
 
     if (callback) {
@@ -35,9 +31,9 @@ class Main {
 
   private checkUsers() {
     this.users.forEach((user: any, index: number) => {
-      const now = moment().tz(user.settings.timezone).format('H:mm');
+      const now = moment().tz(user.settings.timezone).format("H:mm");
 
-      if(user.settings.send_notification_at === now) {
+      if (user.settings.send_notification_at === now) {
         console.log(`Sending events to user with id ${index}`);
 
         this.sendCalendarEvents(user);
@@ -49,12 +45,12 @@ class Main {
     try {
       const events = await this.getCalendarEvents(user);
 
-      if(events.length > 0) {
-        const message = events.reduce(this.calendarEventReducer.bind(this), '')
+      if (events.length > 0) {
+        const message = events.reduce(this.calendarEventReducer.bind(this), "");
 
         await this.telegramApi.sendMessage(user.telegram_chat_id, message);
       } else {
-        console.info('No events found');
+        console.info("No events found");
       }
     } catch (err) {
       console.error(err);
@@ -64,17 +60,19 @@ class Main {
   private getCalendarEvents(user: any) {
     const now = moment().tz(user.settings.timezone);
 
-    const start = now.startOf('day').toISOString();
-    const end = now.endOf('day').toISOString();
+    const start = now.startOf("day").toISOString();
+    const end = now.endOf("day").toISOString();
 
-    return this.getAccessToken(user.refresh_token)
-      .then(calendarApi => calendarApi.getEvents(start, end));
+    return this.getAccessToken(user.refresh_token).then((calendarApi) =>
+      calendarApi.getEvents(start, end)
+    );
   }
 
   private getAccessToken(refreshToken: string) {
-    return this.authApi.refreshToken(refreshToken)
-      .then(data => data.access_token)
-      .then(accessToken => new CalendarApi(accessToken, fetch))
+    return this.authApi
+      .refreshToken(refreshToken)
+      .then((data) => data.access_token)
+      .then((accessToken) => new CalendarApi(accessToken, fetch));
   }
 
   private calendarEventReducer(accumulator: string, event: any) {
@@ -87,7 +85,7 @@ class Main {
       body += `<b>Description:</b> ${event.description} ${os.EOL}`;
     }
 
-    body += os.EOL
+    body += os.EOL;
 
     return accumulator + body;
   }
@@ -100,10 +98,10 @@ class Main {
         dateTimeMoment = dateTimeMoment.tz(obj.timeZone);
       }
 
-      return dateTimeMoment.format('D MMM H:mm:ss');
+      return dateTimeMoment.format("D MMM H:mm:ss");
     }
 
-    return moment(obj.date).format('D MMM');
+    return moment(obj.date).format("D MMM");
   }
 }
 
